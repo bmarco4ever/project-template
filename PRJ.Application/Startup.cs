766ed64a82da33bf054fc18de1232bec0139fd41
Marco S Ferreira <bmarco4ever@gmail.com>
2020-07-23 20:46:43 -0300

    Adicionado Token Bearer no Swagger.

--- PRJ.Application/Startup.cs
+++ PRJ.Application/Startup.cs
@@ -79,7 +79,7 @@ namespace PRJ.Application
                     c.SwaggerDoc("v1", new OpenApiInfo
                     {
                         Version = "v1",
-                        Title = "ToDo API",
+                        Title = "Template API",
                         Description = "A simple example ASP.NET Core Web API",
                         TermsOfService = new Uri("https://example.com/terms"),
                         Contact = new OpenApiContact
@@ -92,6 +92,30 @@ namespace PRJ.Application
                         {
                             Name = "Use under LICENSE",
                             Url = new Uri("https://example.com/license"),
+                        }
+                    });
+
+                    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                    {
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer",
+                        In = ParameterLocation.Header,
+                        Description = @"JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token in the text input below. Example: 'Bearer 12345abcdef'"
+                    });
+
+                    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
                         }
                     });
                 });
@@ -118,7 +142,7 @@ namespace PRJ.Application
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-                c.DocumentTitle = "Todo APIs";
+                c.DocumentTitle = "Template APIs";
                 c.RoutePrefix = string.Empty;
             });
 
